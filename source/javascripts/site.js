cd457c893ed1300b64d6a25b204f4e76e75f3a69
HongdaLin <lin.3235@osu.edu>
2021-07-08 20:40:17 -0400

    some inital stuffs for js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,26 @@
-// This is where it all goes :)
+/*
+Definition: when user click AC the display screen should display 0 and memory ...
+ */
+document.getElementById("clear").addEventListener("click", function (){
+    document.getElementsByClassName("calculator_display")[0].innerHTML = 0;
+}, false);
+
+
+
+
+
+
+/*
+Definition: when user click digit button the display screen changes.
+ */
+var digits = document.getElementsByClassName("digit");
+for(var i=0; digits.length; i++){
+    digits[i].addEventListener("click",function (){
+        document.getElementsByClassName("calculator_display")[0].innerHTML = this.value;
+    }, false);
+}
+/*
+function updateScreen(){
+    document.getElementsByClassName("calculator_display")[0].innerHTML = this.value;
+}
+*/
