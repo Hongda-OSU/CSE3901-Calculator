93defaf9e7219acebdcfd776226c5ebf380d5c29
Samuel Gernstetter <gernstetter.3@osu.edu>
2021-07-08 21:55:27 -0400

    Add event listeners for arithmetic operators

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -24,3 +24,25 @@ function updateScreen(){
     document.getElementsByClassName("calculator_display")[0].innerHTML = this.value;
 }
 */
+
+/* Created 7/8/21 by Samuel Gernstetter */
+function addition() {
+
+}
+function subtraction() {
+
+}
+function multiplication() {
+
+}
+function division() {
+
+}
+let plus = document.getElementById("plus")
+plus.addEventListener("click", addition, false)
+let minus = document.getElementById("minus")
+minus.addEventListener("click", subtraction, false)
+let times = document.getElementById("times")
+times.addEventListener("click", multiplication, false)
+let divide = document.getElementById("divide")
+divide.addEventListener("click", division, false)
