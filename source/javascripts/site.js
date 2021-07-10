085a2bfa8cbf3f6f53cd8241dbb920fca63ca4c6
Samuel Gernstetter <gernstetter.3@osu.edu>
2021-07-10 00:53:48 -0400

    Remove some unnecessary value attributes, edit code to use names and innerHTML, change some vars to lets, remove some incorrect lets

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,8 +2,8 @@
 /*
 Definition: when user click AC the display screen should display 0 and memory ...
  */
-document.getElementById("clear").addEventListener("click", function (){
-    document.getElementsByClassName("calculator_display")[0].innerHTML = 0;
+document.getElementsByName("clear")[0].addEventListener("click", function (){
+    document.getElementsByClassName("calculator_display")[0].innerHTML = "0";
 }, false);
 
 
@@ -14,8 +14,8 @@ Definition: when user click digit button the display screen changes.
 //Created on ___ by Hongda Lin
 //Edited 7/9/21 by Madison Graziani
 //   -Changed second parameter
-var digits = document.getElementsByClassName("digit");
-for(var i=0; i < digits.length; i++){
+digits = document.getElementsByName("digit");
+for (let i = 0; i < digits.length; i++){
     digits[i].addEventListener("click", updateDigits, false);
 }
 
@@ -27,22 +27,21 @@ for(var i=0; i < digits.length; i++){
 //   -Added check for leading zero
 //   -Added comma separators
 function updateDigits(){
-    var newVal;
-    var currVal = document.getElementsByClassName("calculator_display")[0].innerHTML;
-        if(currVal !== "0") {
-            newVal = currVal + this.value;
-            newVal = Array.from(newVal);
-            newVal = newVal.filter(function(val) {return val !== ","})
-            var index = newVal.length - 3;
-            while(index > 0){
-                newVal.splice(index, 0, ",");
-                index = index - 3;
-            }
-            document.getElementsByClassName("calculator_display")[0].innerHTML = newVal.join("");
+    let newVal;
+    let currVal = document.getElementsByClassName("calculator_display")[0].innerHTML;
+    if (currVal !== "0") {
+        newVal = currVal + this.innerHTML;
+        newVal = Array.from(newVal);
+        newVal = newVal.filter(function(val) {return val !== ","})
+        let index = newVal.length - 3;
+        while (index > 0) {
+            newVal.splice(index, 0, ",");
+            index = index - 3;
         }
-        else{
-            document.getElementsByClassName("calculator_display")[0].innerHTML = this.value;
-        }
+        document.getElementsByClassName("calculator_display")[0].innerHTML = newVal.join("");
+    } else {
+        document.getElementsByClassName("calculator_display")[0].innerHTML = this.innerHTML;
+    }
 }
 
 
@@ -101,29 +100,29 @@ function division() {
     currentOperator = "division";
 }
 
-operators = document.getElementsByClassName("operator");
-operators.namedItem("plus").addEventListener("click", addition, false);
-operators.namedItem("minus").addEventListener("click", subtraction, false);
-operators.namedItem("times").addEventListener("click", multiplication, false);
-operators.namedItem("divide").addEventListener("click", division, false);
+operators = document.getElementsByName("operator");
+operators[7].addEventListener("click", addition, false);
+operators[6].addEventListener("click", subtraction, false);
+operators[5].addEventListener("click", multiplication, false);
+operators[4].addEventListener("click", division, false);
 
 /*Created by Drew Jackson 7/9/21*/
 /*
     Object holding stored value in memory
  */
-const memory = {
+memory = {
     digits: 0
 }
 
 //Listeners for each memory button
 
-let ms = document.getElementById("ms");
+ms = document.getElementById("ms");
 ms.addEventListener("click", memory_store(memory), false);
 
-let mr = document.getElementById('mr');
+mr = document.getElementById('mr');
 mr.addEventListener("click", memory_recall, false);
 
-let mplus = document.getElementById("mplus");
+mplus = document.getElementById("mplus");
 mplus.addEventListener("click", memory_add, false);
 
 //TODO add buttons
